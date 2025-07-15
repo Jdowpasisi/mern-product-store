cdae7d10a4880a5cbe948b5ad4948a8971a20165
Ark Mayukh <143125051+Jdowpasisi@users.noreply.github.com>
2025-07-15 21:43:41 +0000

    frontend initialised

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/products", productRoutes)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
     console.log('Server is running on port 5000');
 });
