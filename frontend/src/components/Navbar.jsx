7dfbf3a0a784466b08dc27650d522cd216a8f12e
Ark Mayukh <143125051+Jdowpasisi@users.noreply.github.com>
2025-07-16 19:43:19 +0000

    Home Page  and store

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,44 +1,51 @@
-import { Button, Container, Flex, HStack, Text, useColorMode } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-import { PlusSquareIcon } from '@chakra-ui/icons'
-import { IoMoon } from 'react-icons/io5'
-import { LuSun} from 'react-icons/lu';
+import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
     const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Container maxW = {"1140px"} px={4}>
-        <Flex 
-            h={16}
-            alignItems={"center"}
-            justifyContent={"space-between"}
+    <Container maxW={"1140px"} py={4}>
+        <Flex
+            h={16} 
+            justifyContent="space-between" 
+            alignItems="center"
             flexDir={{
                 base: "column",
-                sm: "row"
+                sm: "row",
             }}
-        >
-            <Text 
-                fontSize={{ base: "22", sm: "28"}}
-                fontWeight={"bold"}
-                textTransform={"uppercase"}
-                textAlign={"center"}
-                bgGradient={'linear(to-r, cyan.500, blue.500)'}
-                bgClip={'text'}
+            >
+                <Text
+                    fontSize={{ base: "22", sm: "28"}}
+                    fontWeight={"bold"}
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
                 >
-                    <Link to="/">Product Store 🛒</Link>
-            </Text>
+                    <Link to={"/"}>Product Store</Link>
+                </Text>
                 <HStack spacing={2} alignItems={"center"}>
-                    <Link to="/create">
+                    <Link to={"/create"}>
                         <Button>
-                            <PlusSquareIcon fontSize={20}/>
+                            <PlusSquareIcon fontSize={"20"} />
                         </Button>
                     </Link>
                     <Button onClick={toggleColorMode}>
-                        {colorMode === 'light' ? <IoMoon/> : <LuSun size="20"/>}
+                        {colorMode === "light" ? (
+                            <IoMoon fontSize={"20"} />
+                        ) : (
+                            <LuSun fontSize={"20"} />
+                        )}
                     </Button>
                 </HStack>
         </Flex>
     </Container>
+                        
+
   )
 }
 
